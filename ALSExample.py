59a37c29f4cd956b9f09e29e8f97b490b99ff1ae
vaggelis <vaggelis@Evangeloss-MacBook-Pro.local>
2015-09-25 22:35:38 +0300

    changes to source

---
+++ ALSExample.py
@@ -0,0 +1,62 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import sys,json
+
+from pyspark import SparkContext
+from pyspark.mllib.recommendation import ALS
+
+"""
+ALSExample.py - 25/9/2015 - Evangelos Tripolitakis
+vtripolitakis@gmail.com
+
+$ cd $SPARK_HOME
+$ ./bin/spark-submit --master local[*]  \
+> /path_to/ALSExample.py /path_to/ratings_file /path_to/output_files 
+
+"""
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 3:
+        print("""
+        first argument is ratings file and second is output file
+        """)
+        exit(-1)
+
+    txtFile = sys.argv[1]
+    outputFile = sys.argv[2]
+
+    sc = SparkContext(appName="ALSExample")
+    ratings = sc.textFile(txtFile)
+    processedRatings = ratings.map(lambda line: line.split(","))
+    users = processedRatings.map(lambda rating: (int(rating[0]),1)).reduceByKey(lambda a,b:a+b).map(lambda user: user[0]).collect()
+
+    #train model
+    model = ALS.trainImplicit(processedRatings, 1,seed=10)
+
+    outArray=[]
+    f=open(outputFile,'w')
+    
+    for user in users:        
+        outArray.append(model.recommendProducts(user,20))
+
+    f.write(json.dumps(outArray))
+
+    sc.stop()
+    f.close()
